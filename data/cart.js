13200182e00a82dbff2e2e38b96daec8d19fc336
Lidor Sanker <lidor7776@gmail.com>
2025-05-18 13:58:11 +0300

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,8 +1,18 @@
 export let cart = JSON.parse(localStorage.getItem("cart"));
+console.log(cart);
+
 if (!cart) {
-  [
-    { productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", quantity: 2 },
-    { productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d", quantity: 1 },
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+      deliveryOptionId: "1",
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+      deliveryOptionId: "2",
+    },
   ];
 }
 
@@ -25,6 +35,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
+      deliveryOptionId: "1",
     });
   }
 
