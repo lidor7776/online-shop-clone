14540c2f49647ef195737d324de854ecba669bf0
Lidor Sanker <lidor7776@gmail.com>
2025-05-26 13:12:42 +0300

    Testing renderOrderSummary

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -7,18 +7,23 @@ describe("test suite:addToCart", () => {
       return JSON.stringify([
         {
           productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-          quantity: 1,
+          quantity: 2,
           deliveryOptionId: "1",
+        },
+        {
+          productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+          quantity: 1,
+          deliveryOptionId: "2",
         },
       ]);
     });
     loadFromStorage();
 
     addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
-    expect(cart.length).toEqual(1);
+    expect(cart.length).toEqual(2);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
-    expect(cart[0].quantity).toEqual(2);
+    expect(cart[0].quantity).toEqual(3);
   });
 
   it("adds a new product to the cart", () => {
